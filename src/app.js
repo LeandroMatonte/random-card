0524e7ea304ad9a64cdcebe2bac1f52ac19aeae7
LeandroMatonte <leandromatonte99@gmail.com>
2021-04-23 18:47:43 +0000

    Version final

--- src/app.js
+++ src/app.js
@@ -6,6 +6,58 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function generarCarta() {
+    let palos = ["♣", "♦", "♥", "♠"];
+    let numero = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+
+    let objetoPalos = document.getElementsByClassName("palo");
+    let objetoNumero = document.querySelector(".numero");
+
+    let palo = palos[Math.floor(Math.random() * 4)];
+
+    if (palo == "♥" || palo == "♦") {
+      objetoPalos[0].classList.add("rojo");
+      objetoPalos[1].classList.add("rojo");
+    } else {
+      objetoPalos[0].classList.remove("rojo");
+      objetoPalos[1].classList.remove("rojo");
+    }
+
+    objetoPalos[0].innerHTML = objetoPalos[1].innerHTML = palo;
+    objetoNumero.innerHTML = numero[Math.floor(Math.random() * 13)];
+  }
+
+  generarCarta();
+
+  //parte opcional boton
+  let btnGenerator = document.querySelector("#btnGenerator");
+  btnGenerator.addEventListener("click", () => {
+    generarCarta();
+  });
+
+  //parte opcional input
+  let btnChange = document.querySelector("#btnChange");
+  btnChange.addEventListener("click", () => {
+    let width = document.querySelector("#cardWidth");
+    let height = document.querySelector("#cardHeight");
+    let card = document.querySelector(".card");
+    if (height.value >= 600) {
+      card.style.height = height.value + "px";
+    }
+    card.style.width = width.value + "px";
+  });
 };
